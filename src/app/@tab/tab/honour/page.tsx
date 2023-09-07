2f7b28ed6b9ab72b418da36f64313bf9309e1e65
jiangyp <jiangyp@chinawyny.com>
2023-09-07 15:28:37 +0800

    fix:  ts error

--- src/app/@tab/tab/honour/page.tsx
+++ src/app/@tab/tab/honour/page.tsx
@@ -60,10 +60,9 @@ export default function Page() {
       <TabCard styles="w-[471px] h-[646px] pt-16">
         {institutions.map((m) => {
           return (
-            <div className="flex px-5 text-lg mt-4">
+            <div key={m} className="flex px-5 text-lg mt-4">
               <Image
-                key={m}
-                className='mr-2'
+                className="mr-2"
                 src={'/honor/icon.png'}
                 width={24}
                 height={24}
@@ -76,15 +75,12 @@ export default function Page() {
       </TabCard>
       {list.map((m) => {
         return (
-          <div className="flex flex-col w-[396px] h-[652px] border border-primary ml-6">
+          <div
+            key={m.key}
+            className="flex flex-col w-[396px] h-[652px] border border-primary ml-6"
+          >
             <div className="flex flex-1 justify-center items-center">
-              <Image
-                key={m.key}
-                src={m.src}
-                width={m.width}
-                height={m.height}
-                alt={''}
-              />
+              <Image src={m.src} width={m.width} height={m.height} alt={''} />
             </div>
             <div className="flex h-[110px] justify-center items-center">
               <span className="text-primary inline-block w-3/4 text-center">
