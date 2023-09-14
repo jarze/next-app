ccb508c80deed99659b97221447af402aa9d8b31
jiangyp <jiangyp@chinawyny.com>
2023-09-14 14:32:12 +0800

    feat: 智能制造

--- src/app/@tab/tab/business-intelligent-manufacturing/page.tsx
+++ src/app/@tab/tab/business-intelligent-manufacturing/page.tsx
@@ -1,61 +1,68 @@
 import Image from 'next/image';
-
-const list = [
-  {
-    src: '/business/introduce_1.png',
-    width: 437,
-    height: 619,
-  },
-  {
-    src: '/business/introduce_2.png',
-    width: 482,
-    height: 619,
-    className: '-ml-10',
-  },
-  {
-    src: '/business/introduce_3.png',
-    width: 321,
-    height: 619,
-    className: '-ml-20',
-  },
-
-  {
-    src: '/business/introduce_4.png',
-    width: 321,
-    height: 619,
-    className: 'ml-[300px]',
-  },
-  {
-    src: '/business/introduce_5.png',
-    width: 463,
-    height: 619,
-    className: '-ml-20',
-  },
-];
+import TabCard from '@/components/TabCard';
 
 export default function Page() {
   return (
     <div className="flex h-full items-center justify-center">
-      {list.map((m) => {
-        return (
-          <Image className={m.className} key={m.src} {...m} alt={'arrow'} />
-        );
-      })}
-
-      <div className="flex flex-col items-center justify-center w-[559px] h-[559px] left-[1400px] absolute border border-primary shadow-[inset_0_0_32px_1px] shadow-primary rounded-full">
-        <Image
-          src="/business/introduce_logo.png"
-          width={321}
-          height={129}
-          alt={'arrow'}
-        />
-         <Image
-         className='mt-8'
-          src="/business/introduce_title.png"
-          width={224}
-          height={68}
-          alt={'arrow'}
-        />
+      <div className="flex w-full overflow-x-auto">
+        <TabCard styles="flex-none w-[1083px] h-[627px] px-[27px] py-14 mr-12">
+          <div className="text-primary text-[40px] leading-[60px] ">
+            2021年3月12日，《中华人民共和国经济和社会发展
+            第十四个五年规划和2035年远景目标纲要》
+          </div>
+          <div className="flex mt-20">
+            <Image
+              className="h-[251px]"
+              src="/business/intelligent_manufacturing_1.png"
+              width={416}
+              height={251}
+              alt={''}
+            />
+            <div className="text-primary text-[24px] leading-10 ml-12">
+              “加快数字化发展，建设数字中国”中明确提出打造数字经济新优势：充分发挥海量数据和丰富应用场景优势，促进数字技术与实体经济深度融合，赋能传统产业转型升级，催生新产业新业态新模式，壮大经济发展新引擎。实施“上云用数赋智”行动，推动数据赋能全产业链协同转型。
+            </div>
+          </div>
+        </TabCard>
+        <TabCard styles="flex-none w-[1083px] h-[627px] px-[27px] py-14 mr-12">
+          <Image
+            src="/business/intelligent_manufacturing_2.png"
+            width={1029}
+            height={515}
+            alt={''}
+          />
+        </TabCard>
+        <TabCard styles="flex-none w-[1083px] h-[627px] px-[27px] py-14 mr-12">
+          <Image
+            src="/business/intelligent_manufacturing_3.png"
+            width={1029}
+            height={515}
+            alt={''}
+          />
+        </TabCard>
+        <TabCard styles="flex-none w-[1083px] h-[627px] px-[27px] py-14 mr-12">
+          <Image
+            src="/business/intelligent_manufacturing_4.png"
+            width={1029}
+            height={515}
+            alt={''}
+          />
+        </TabCard>
+        <TabCard styles="flex-none w-[1083px] h-[627px] px-[27px] py-14 mr-12">
+          <Image
+            src="/business/intelligent_manufacturing_5.png"
+            width={1029}
+            height={515}
+            alt={''}
+          />
+        </TabCard>
+        <TabCard styles="flex-none w-[1083px] h-[627px] px-[27px] py-14 mr-12">
+          <Image
+            src="/business/intelligent_manufacturing_6.png"
+            width={1029}
+            height={515}
+            alt={''}
+          />
+        </TabCard>
       </div>
     </div>
   );
