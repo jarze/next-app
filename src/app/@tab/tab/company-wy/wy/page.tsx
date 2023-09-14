1216f682e6685e2ef6f8f73420b39a4fd882735a
jiangyp <jiangyp@chinawyny.com>
2023-09-14 16:22:36 +0800

    feat: 使命愿景

--- src/app/@tab/tab/company-wy/wy/page.tsx
+++ src/app/@tab/tab/company-wy/wy/page.tsx
@@ -8,17 +8,24 @@ import Vision from './components/Vision';
 export default function Page() {
   const [value, setValue] = useState('1');
   return (
-    <Tabs
-      activeKey={value}
-      isCache={true}
-      onChange={(value) => setValue(value)}
+    <div
+      data-no-wrapper
+      className="bg-[url('/company/bg_vision_border.png')] h-[898px] w-[3107px] bg-[length:100%_100%] overflow-hidden pt-1"
     >
-      <Tabs.Panel tabKey="1" tab={'万益能源'}>
-        <Introduction />
-      </Tabs.Panel>
-      <Tabs.Panel tabKey="2" tab={'使命愿景'}>
-        <Vision />
-      </Tabs.Panel>
-    </Tabs>
+      <div className="bg-[url('/company/bg_vision.png')] h-[886px] w-[3102px] bg-cover pt-[75px] pl-[100px]">
+        <Tabs
+          activeKey={value}
+          isCache={true}
+          onChange={(value) => setValue(value)}
+        >
+          <Tabs.Panel tabKey="1" tab={'万益能源'}>
+            <Introduction />
+          </Tabs.Panel>
+          <Tabs.Panel tabKey="2" tab={'使命愿景'}>
+            <Vision />
+          </Tabs.Panel>
+        </Tabs>
+      </div>
+    </div>
   );
 }
