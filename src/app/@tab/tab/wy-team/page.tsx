c70f93e84a4e3b7ff0675f71a2729c7ed6038a9a
jiangyp <jiangyp@chinawyny.com>
2023-09-15 14:44:15 +0800

    fix: 人才团队样式

--- src/app/@tab/tab/wy-team/page.tsx
+++ src/app/@tab/tab/wy-team/page.tsx
@@ -21,26 +21,26 @@ export default function Page() {
           </div>
           <div className="flex flex-col items-center">
             <div className="w-[200px] h-[200px] rounded-full bg-primary text-center leading-[200px] font-bold text-[55px] text-[#030322]">
-              95.7%
+              30.4%
             </div>
             <span className="inline-block mt-[20px]">硕士</span>
           </div>
           <div className="flex flex-col items-center">
             <div className="w-[200px] h-[200px] rounded-full bg-primary text-center leading-[200px] font-bold text-[55px] text-[#030322]">
-              95.7%
+              10.4%
             </div>
             <span className="inline-block mt-[20px]">博士</span>
           </div>
           <div className="flex flex-col items-center">
             <div className="w-[200px] h-[200px] rounded-full bg-primary text-center leading-[200px] font-bold text-[55px] text-[#030322]">
-              95.7%
+              5.2%
             </div>
             <span className="inline-block mt-[20px]">海外留学背景</span>
           </div>
         </div>
         <div className='px-20 mt-28 leading-10'>
           <p>
-            万益能源顺应国家政策趋势，不断探索业务转型升级，增强自主研发和创新能力，搭建了以客户需求为核心、以业务为导向的复合式人才团队。
+            万益能源顺应国家政策趋势，不断探索业务转型升级，增强自主研发和创新能力，<span className='text-primary'>搭建了以客户需求为核心、以业务为导向的复合式人才团队。</span>
           </p>
           <p>
             公司拥有超过50%的产研团队，团队成员来自中国、美国、法国、德国、香港等知名企业和高校，专业背景横跨电气、信息工程、人工智能、数学、金融等领域。团队成员本科生及以上占比超过95.7%，其中，硕士博士占比分别达到30%和5%以上，此外，还有10%拥有海外留学背景。
