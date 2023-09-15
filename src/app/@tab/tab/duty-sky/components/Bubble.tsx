40eae567b0226233f509411b79f69756875a5845
jiangyp <jiangyp@chinawyny.com>
2023-09-15 13:36:30 +0800

    fix: 动画

--- src/app/@tab/tab/duty-sky/components/Bubble.tsx
+++ src/app/@tab/tab/duty-sky/components/Bubble.tsx
@@ -2,7 +2,7 @@ export default function Bubble({ className = '', children = null }: any) {
   return (
     <div
       className={
-        ' flex flex-col absolute justify-center items-center rounded-full shadow-[inset_0_0_32px_1px_#00ffee] ' +
+        ' flex flex-col animate-drop absolute justify-center items-center rounded-full shadow-[inset_0_0_32px_1px_#00ffee] ' +
         className
       }
     >
